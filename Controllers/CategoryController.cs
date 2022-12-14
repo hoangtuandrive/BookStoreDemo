a03c15313c7af4b7e2200c3763aafd330687b486
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-12-14 17:40:13 +0700

    First commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //get all category
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
