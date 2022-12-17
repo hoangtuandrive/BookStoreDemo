eee6f1609762be280667cbfb6cb614f62e0abfd5
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-12-17 18:45:14 +0700

    Edit category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -41,5 +41,42 @@ namespace BookStoreWeb.Controllers
             }
             return View(obj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(); //Return exception if have more than 1 elements
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id); 
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot match the Name");
+                ModelState.AddModelError("displayorder", "The Display Order cannot match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
