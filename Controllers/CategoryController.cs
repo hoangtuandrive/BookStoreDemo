3cad7e2f7554e7fe1d6080610dc9cddf71458755
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-12-17 21:09:35 +0700

    Delete category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -78,5 +78,38 @@ namespace BookStoreWeb.Controllers
             }
             return View(obj);
         }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(); //Return exception if have more than 1 elements
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id); 
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();  
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
